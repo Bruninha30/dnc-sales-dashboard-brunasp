e4117c19efacf0b64e22c1fe03f1e82f63885550
Bruna Rodrigues <brunasp3008@gmail.com>
2025-08-21 21:22:49 -0400

    Feat: Addinng Styled Components

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,10 @@
+function Profile() {
+  return (
+    <>
+      <h1>Leads Page</h1>
+      <p>This page will display leads information.</p>
+      <p>More features coming soon!</p>
+    </>
+  )
+}
+export default Profile
