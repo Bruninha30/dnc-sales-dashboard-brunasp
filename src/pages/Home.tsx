81cae0058e9e869efccf1c077d05a723f9e5b015
Bruna Rodrigues <brunasp3008@gmail.com>
2025-09-11 15:49:32 -0400

    feat: Avatars List Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,32 @@
-import { Header } from '@/components'
+import { AvatarsList, CardComponent, Header } from '@/components'
+import { Container } from '@mui/material'
+import { currencyConverter } from '@/utils'
+
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/Avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(1234.54),
+    },
+    {
+      avatar: '/Avatar.svg',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencyConverter(2345.43),
+    },
+    {
+      avatar: '/Avatar.svg',
+      name: 'Nome Sobrenome 3',
+      subtitle: currencyConverter(3248.43),
+    },
+  ]
   return (
     <>
       <Header />
-      <h1>Home</h1>
+      <Container maxWidth="lg">
+        <CardComponent>CARD</CardComponent>
+        <AvatarsList listData={mockListData} />
+      </Container>
     </>
   )
 }
